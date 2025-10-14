471f8c9061f63985ee3adff76f2db5913a96af1c
¨BrunoLisboaSilva¨ <bruno.lisboa.silva@outlook.com>
2025-10-14 10:33:41 -0300

    fix(api): ensure PATCH method is available for task updates

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,8 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-Route::get('/tasks', [TaskController::class, 'index']);
-Route::post('/tasks', [TaskController::class, 'store']);
-Route::get('/tasks/{id}', [TaskController::class, 'show']);
-Route::put('/tasks/{id}', [TaskController::class, 'update']);
-Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+Route::apiResource('tasks', TaskController::class);
+
